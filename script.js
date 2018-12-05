ab214a2d4ca34e3758884909cd68358d540f1c4f
JackYang555 <43828896+JackYang555@users.noreply.github.com>
2018-12-05 17:07:24 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".button").click(function() {
+    $(".dead").fadeOut();
+    $(".alive").fadeIn();
+    $(".dead").fadeIn();
+    $(".alive").fadeOut();
+});
+
+$(".button2").click(function() {
+    $(".Castle").fadeOut();
+    $(".Castle").fadeIn(); 
+});
+
+$(".button3").click(function() {
+    $(".Unicorn").fadeOut();
+});
